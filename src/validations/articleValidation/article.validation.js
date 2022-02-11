bd1c091c5a554e814827d1ab2438e03de6d86696
Tsa2341 <alanshema2002@gmail.com>
2022-02-11 14:57:58 +0200

    bug: debbugging github action

--- src/validations/articleValidation/article.validation.js
+++ src/validations/articleValidation/article.validation.js
@@ -2,8 +2,8 @@ import { articleSchema } from "./article.schema.js";
 
 export const articleValidation = async (req, res, next) => {
   const value = articleSchema.validate(req.body);
-  console.log(value);
   if (value.error) {
+    console.log(value);
     res.status(406).json({
       message: value.error.details[0].message.replaceAll('"', ""),
     });
