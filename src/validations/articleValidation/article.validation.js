fa29f9cc4abd1b8c1f3c01ee9822d33c9cd5626c
Tsa2341 <alanshema2002@gmail.com>
2022-02-11 15:45:21 +0200

    chore: some minor update on swagger

--- src/validations/articleValidation/article.validation.js
+++ src/validations/articleValidation/article.validation.js
@@ -3,13 +3,16 @@ import { articleSchema } from "./article.schema.js";
 export const articleValidation = async (req, res, next) => {
   const value = articleSchema.validate(req.body);
   if (value.error) {
-    console.log("in article validations = ", value.error.details[0]);
+    console.log(
+      "in article validations = ",
+      value.error.details[0].message.replaceAll('"', "")
+    );
     try {
       res.status(406).json({
-        message: value.error.details[0].message.replaceAll('"', ""),
+        message: value.error,
       });
     } catch (error) {
-      res.status(406).json({
+      res.status(404).json({
         message: error,
       });
     }
